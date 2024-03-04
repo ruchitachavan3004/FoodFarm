17c815cb60ad14fdf75856c6b46b1b5b45c301a5
ruchi <ruchitachavan3004@gmail.com>
2024-03-04 15:15:25 +0530

    first commit

---
+++ loginpage/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './Components/LoginPage';
+import RegisterForm from './Components/SignUpPage';
+import HomePage from './Components/HomePage';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div>
+         
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ loginpage/src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './HomePage.css';
+import img1 from '../images/green3.png';
+
+const HomePage = () => {
+ return(
+    <div class="body-home">
+        <img src='green3.png' alt="nature"></img>
+
+    </div>
+ );
+};
+
+export default HomePage;
